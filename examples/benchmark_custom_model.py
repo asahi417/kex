c6c8aadb6100144096a99d52e9fb7fcac019e85d
asahi417 <asahi1992ushio@gmail.com>
2020-11-27 10:59:26 +0000

    add new result

--- examples/benchmark_custom_model.py
+++ examples/benchmark_custom_model.py
@@ -1,7 +1,7 @@
-""" Benchmark custom method """
+""" Get top 10 micro F1 score for a custom method with Inspec dataset """
 import logging
 import json
-from time import time
+from random import shuffle
 from tqdm import tqdm
 
 import grapher
@@ -10,10 +10,10 @@ logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s', level=logg
 
 
 class CustomExtractor:
-    """ Custom keyword extractor example: First N keywords extractor """
+    """ Custom keyword extractor example: random extractor """
 
     def __init__(self, maximum_word_number: int = 3):
-        """ First N keywords extractor """
+        """ random keywords extractor """
         self.phrase_constructor = grapher.PhraseConstructor(maximum_word_number=maximum_word_number)
 
     def get_keywords(self, document: str, n_keywords: int = 10):
@@ -30,40 +30,33 @@ class CustomExtractor:
         eg) {'stemmed': 'grid comput', 'pos': 'ADJ NOUN', 'raw': ['grid computing'], 'offset': [[11, 12]], 'count': 1}
         """
         phrase_instance, stemmed_tokens = self.phrase_constructor.tokenize_and_stem_and_phrase(document)
-        sorted_phrases = sorted(phrase_instance.values(), key=lambda x: x['offset'][0][0])
-        return sorted_phrases[:min(len(sorted_phrases), n_keywords)]
+        phrase_list = list(phrase_instance.values())
+        shuffle(phrase_list)
+        return phrase_list[:min(len(phrase_list), n_keywords)]
 
 
 if __name__ == '__main__':
     model = CustomExtractor()
-    data_name = 'Inspec'
-
-    data, _ = grapher.get_benchmark_dataset(data_name)
+    data, _ = grapher.get_benchmark_dataset('Inspec')
 
     # run algorithm and test it over data
-    tp = {"5": 0, "10": 0, "15": 0}
-    fn = {"5": 0, "10": 0, "15": 0}
-    fp = {"5": 0, "10": 0, "15": 0}
+    tp, fn, fp = 0, 0, 0
 
-    start = time()
+    topn = 5
     for v in tqdm(data):
         source = v['source']
         gold_keys = v['keywords']   # already stemmed
         # inference
-        keys = model.get_keywords(source, n_keywords=15)
+        keys = model.get_keywords(source, n_keywords=topn)
         keys_stemmed = [k['stemmed'] for k in keys]
-        for i in [5, 10, 15]:
-            positive_answers = list(set(keys_stemmed[:i]).intersection(set(gold_keys)))
-            tp[str(i)] += len(positive_answers)
-            fn[str(i)] += len(gold_keys) - len(positive_answers)
-            fp[str(i)] += i - len(positive_answers)
-    elapsed = time() - start
+        positive_answers = list(set(keys_stemmed[:topn]).intersection(set(gold_keys)))
+        tp += len(positive_answers)
+        fn += len(gold_keys) - len(positive_answers)
+        fp += topn - len(positive_answers)
+
     # result summary: micro F1 score
-    result_json = {'process_time_second': elapsed}
-    for i in [5, 10, 15]:
-        precision = tp[str(i)]/(fp[str(i)] + tp[str(i)])
-        recall = tp[str(i)]/(fn[str(i)] + tp[str(i)])
-        f1 = 2 * precision * recall / (precision + recall)
-        result_json['top_{}'.format(i)] = {"mean_recall": recall, "mean_precision": precision, "f_1": f1}
-    logging.info(json.dumps(result_json, indent=4, sort_keys=True))
+    precision = tp/(fp + tp)
+    recall = tp/(fn + tp)
+    f1 = 2 * precision * recall / (precision + recall)
+    logging.info(json.dumps({"precision": precision, "recall": recall, "f1": f1}, indent=4, sort_keys=True))
 
