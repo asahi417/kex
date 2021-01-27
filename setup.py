8ced8220081c917420dc6f456d1813c9fb661d7c
asahi417 <asahi1992ushio@gmail.com>
2021-01-27 17:25:06 +0000

    LISCENCE

--- setup.py
+++ setup.py
@@ -2,19 +2,29 @@ from setuptools import setup, find_packages
 
 VERSION = '0.0.0'
 NAME = 'kex'
-IS_RELEASED = False
 
 with open('README.md') as f:
     readme = f.read()
 
 setup(
     name=NAME,
+    packages=[NAME],
     version=VERSION,
+    license='MIT',
     description='Light/easy keyword extraction.',
+    url='https://github.com/asahi417/kex',
+    download_url='https://github.com/user/reponame/archive/v_01.tar.gz',  # I explain this later on
+    keywords=['keyword-extraction', 'nlp', 'information-retrieval'],
     long_description=readme,
     author='Asahi Ushio',
     author_email='asahi1992ushio@gmail.com',
-    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 4 - Beta',       # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+        'Intended Audience :: Developers',      # Define that your audience are developers
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',   # Again, pick a license
+        'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+      ],
     include_package_data=True,
     test_suite='tests',
     install_requires=[
